63d39ee44dcee6b930faf542fb448e37ab6f2c01
NaqibKhusairy <naaqibkhusairy@gmail.com>
2023-07-07 01:27:19 +0800

    start app

--- app/src/main/java/com/naqib/ictoanything/MainActivity.java
+++ app/src/main/java/com/naqib/ictoanything/MainActivity.java
@@ -2,13 +2,34 @@ package com.naqib.ictoanything;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView penerangn;
+    Button Start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        penerangn=findViewById(R.id.textView2);
+        Start=findViewById(R.id.button);
+        penerangn.setText("This system is used to check your Age, " +
+                "\nDate of Birth, Gender and State of Birth");
+
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {{
+                Toast.makeText(getApplicationContext(), "Welcome to IC TO ANYTHING SYSTEM", Toast.LENGTH_SHORT).show();
+                Intent i= new Intent(getApplicationContext(),getIc.class);
+                startActivity(i);
+            }
+            }
+        });
     }
 }
